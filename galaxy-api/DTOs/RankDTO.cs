bec03505bdbcea10e417619d4375fbdc001dab4e
Sam <samkele@bbd.co.za>
2025-04-11 04:16:55 +0200

    added dtos

---
+++ galaxy-api/DTOs/RankDTO.cs
@@ -0,0 +1,8 @@
+namespace galaxy_api.DTOs;
+
+public class RankDTO {
+    public int Rank_Id {get; set;}
+    public required string Title {get; set;}
+
+    public string Description {get; set;} = string.Empty;
+}
