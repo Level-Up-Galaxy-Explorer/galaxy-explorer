6574f7ea23c3f18f35eaf12cf353bc0ac82feb10
Sam <samkele@bbd.co.za>
2025-04-14 14:45:21 +0200

    added to commands

--- galaxy-cli/Commands/CrewCommands/CrewDetailCommand.cs
+++ galaxy-cli/Commands/CrewCommands/CrewDetailCommand.cs
@@ -1,15 +1,71 @@
 using System.ComponentModel;
+using System.Text.Json;
+using galaxy_cli.DTO.Crews;
+using galaxy_cli.Services;
 using galaxy_cli.Settings;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace galaxy_cli.Commands.CrewCommands;
 
 [Description("Shows detailed information about a specific crew.")]
-public class CrewDetailCommand : Command<EmptyCommandSetting>
+public class CrewDetailCommand : AsyncCommand<IdentifierSettings>
 {
-    public override int Execute(CommandContext context, EmptyCommandSetting settings)
+    private readonly ICrewsService _crewService;
+
+    public CrewDetailCommand(ICrewsService crewsService)
     {
-        
+        _crewService = crewsService;
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, IdentifierSettings settings)
+    {
+        CrewSummaryDTO? crew = null;
+
+        await AnsiConsole.Status()
+        .StartAsync("Calling API...", async ctx =>
+        {
+            try
+            {
+                ctx.Status("Processing...");
+
+                crew = await _crewService.GetCrewItemAsync(settings.Identifier!.Value);
+
+                if (crew == null)
+                {
+
+                    AnsiConsole.MarkupLine("[yellow]Could not deserialize API response.[/]");
+                }
+
+                AnsiConsole.MarkupLine($"[green]API call successful[/]");
+
+
+            }
+            catch (HttpRequestException ex)
+            {
+                AnsiConsole.MarkupLine($"[red]API Request Error:[/]");
+                AnsiConsole.MarkupLine($"[red]({ex.StatusCode?.ToString() ?? "N/A"}) {ex.Message}[/]");
+            }
+            catch (JsonException ex)
+            {
+                AnsiConsole.MarkupLine($"[red]JSON Deserialization Error: {ex.Message}[/]");
+            }
+            catch (Exception ex)
+            {
+                AnsiConsole.MarkupLine($"[red]An unexpected error occurred:[/]");
+                AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+            }
+        });
+
+        if (crew != null)
+        {
+
+            var grid = ConsoleUiHelper.BuildCrewGrid(crew);
+            AnsiConsole.Write(
+            new Panel(grid)
+                .Header("Crew Information"));
+        }
+
         return 0;
     }
 }
