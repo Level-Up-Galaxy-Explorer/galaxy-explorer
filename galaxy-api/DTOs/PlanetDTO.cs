1f75a6a1ab915e3b58b570d153f85373a6632c02
Alfred Malope <alfred.malope@vcontractor.co.za>
2025-04-15 14:08:42 +0200

    Update planet

--- galaxy-api/DTOs/PlanetDTO.cs
+++ galaxy-api/DTOs/PlanetDTO.cs
@@ -2,7 +2,7 @@
 {
     public class PlanetDTO
     {
-        public int Id { get; set; }
+        // public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Galaxy { get; set; } = string.Empty;
         public string PlanetType { get; set; } = string.Empty;
--- galaxy-api/Program.cs
+++ galaxy-api/Program.cs
@@ -4,6 +4,9 @@ using galaxy_api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IPlanetRepository, PlanetRepository>();
 builder.Services.AddScoped<IPlanetService, PlanetService>();
@@ -26,6 +29,11 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
